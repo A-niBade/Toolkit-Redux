bf2de1f2e1c8f72ab1fe917195263b07b3ea22de
Anibal <joan.maldonadoa@gmail.com>
2023-01-19 15:31:24 -0300

    Added pagination

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,24 +1,43 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./store/slices/counter";
 import { getPokemons } from "./store/slices/pokemon";
 
 export const PokemonApp = () => {
   const dispatch = useDispatch();
+  const {
+    isLoading,
+    pokemons = [],
+    page,
+  } = useSelector((state) => state.pokemons);
+
+  const { counter } = useSelector((state) => state.counter);
 
   useEffect(() => {
-    dispatch(getPokemons());
-  }, []);
+    if (counter < 0) return;
+    dispatch(getPokemons(counter));
+  }, [counter]);
 
   return (
     <>
       <h1>POKEMON APP</h1>
       <hr />
 
+      <span>Loading {isLoading ? "True" : "False"}</span>
+
       <ul>
-        <li>HOLA</li>
-        <li>HOLA</li>
-        <li>HOLA</li>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
       </ul>
+
+      <button disabled={isLoading} onClick={() => dispatch(decrement())}>
+        Previous
+      </button>
+
+      <button disabled={isLoading} onClick={() => dispatch(increment())}>
+        Next
+      </button>
     </>
   );
 };
--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -3,13 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: "counter",
   initialState: {
-    counter: 10,
+    counter: 0,
   },
   reducers: {
     increment: (state) => {
       state.counter += 1;
     },
     decrement: (state) => {
+      if (state.counter === 0) return;
       state.counter -= 1;
     },
     incrementBy: (state, action) => {
